3ae4f1f0e66f93b06472ae2e8bb0183d1baec035
Lupesms97 <lupesms97@gmail.com>
2022-11-18 11:23:08 -0300

    Criando banco

---
+++ src/Banco.java
@@ -0,0 +1,17 @@
+public class Banco {
+
+    public static void main(String[] args) {
+        FuncaoBanco funcaoBanco = new FuncaoBanco();
+
+        funcaoBanco.imprimirSaldo();
+
+        funcaoBanco.sacar(100.00);
+
+        funcaoBanco.depositar(350.00);
+
+        
+
+
+    }
+    
+}
