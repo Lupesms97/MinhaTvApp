603de6ee9b92cf140179691cc7c6eb17a4c20dfc
Lupesms97 <lupesms97@gmail.com>
2022-11-18 16:21:46 -0300

    verificcando scanner

--- src/AboutMe.java
+++ src/AboutMe.java
@@ -1,15 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class AboutMe {
     public static void main(String[] args) {
-        
-        String nome = args[0];
-        String sobrenome = args[1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
-        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Digite seu nome:");
+        String nome = scanner.next();
+
+        System.out.println("Qual seu sobrenome:");
+        String sobrenome = scanner.next();
+    
+        System.out.println("Qual a sua idade");
+        int idade = scanner.nextInt();
+
         System.out.println("Olá, eu meu chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos");
         System.out.println("MInha altura é "+ altura);
 
     }
+    
 }
